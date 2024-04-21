2365651f77496a7cbf2790300caab867db694d07
Андрей <Perminand@yandex.ru>
2024-04-21 10:47:32 +0300

    feat: create class

---
+++ src/main/java/ru/yandex/practicum/catsgram/exception/DuplicatedDataException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.catsgram.exception;
+
+public class DuplicatedDataException extends Throwable {
+    public DuplicatedDataException(String s) {
+        super(s);
+    }
+}
