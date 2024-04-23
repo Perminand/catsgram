293b127cc183dc25af0c885d7fc059c679366492
Андрей <Perminand@yandex.ru>
2024-04-23 06:20:16 +0300

    refactor:updatefindPostById

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
@@ -8,28 +9,40 @@ import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
+    private final int SIZE_DEFAULT = 10;
+    private final int FROM_DEFAULT = 5;
+    private final String SORT_DEFAULT = "asc";
 
     @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
     }
 
-    @GetMapping
-    public Collection<Post> findAll() {
-        return postService.findAll();
+    @GetMapping("/search")
+    public Collection<Post> findAll(
+            @RequestParam Optional<Integer> size,
+            @RequestParam Optional<Integer> from,
+            @RequestParam Optional<String> sort) {
+        return postService.findAll(size.orElse(SIZE_DEFAULT),from.orElse(FROM_DEFAULT),sort.orElse(SORT_DEFAULT));
     }
 
     @GetMapping("/{postId}")
-    public Post findById(@PathVariable("postId") Integer postId){
-        return postService.findById(postId).orElseThrow(NullPointerException::new);
+    public Post findPostById(@PathVariable("postId") long postId) throws ConditionsNotMetException {
+        if (postService.findPostById(postId).isPresent()) {
+            return postService.findPostById(postId).get();
+        } else {
+            throw new ConditionsNotMetException("Нет User с заданным id");
+        }
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Post create(@RequestBody Post post) throws ConditionsNotMetException {
         return postService.create(post);
     }
